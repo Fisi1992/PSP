698379e6f53eb87186522f20951b6a9886fb2e34
Fisi1992 <141073491+Fisi1992@users.noreply.github.com>
2024-06-23 22:16:36 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
+// Evento para el formulario de recetas
 document.getElementById('recipe-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Receta agregada con éxito!');
+    e.preventDefault(); // Evitar que el formulario se envíe
+    alert('Receta agregada con éxito!'); // Mostrar mensaje de éxito
 });
 
+// Evento para el formulario de contacto
 document.getElementById('contact-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulario enviado con éxito!');
+    e.preventDefault(); // Evitar que el formulario se envíe
+    alert('Formulario enviado con éxito!'); // Mostrar mensaje de éxito
 });
+
